0714c5b32db2b85017ab8279c122654eb6e7de8b
Le-Rai <rainlazo2006@gmail.com>
2025-10-06 10:54:01 +0800

    Update data_practice.py

--- data_practice.py
+++ data_practice.py
@@ -2,10 +2,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np 
 
-df = pd.read_csv(r"C:\Users\admin\OneDrive\Desktop\Codes\practice_code\weather_data_o.csv", header=2)
+df = pd.read_csv(r"C:\Users\admin\OneDrive\Desktop\Codes\practice_code\weather_data_o.csv", header=2) 
+#changes file location where weather_data_o.csv is located 
+#sets main header at the third row (python starts at zero index)
 
 pd.set_option('display.width', 100)
 pd.set_option('display.max_columns', 6)
+#fixes data display
 
 start_row = 0
 end_row = 24
@@ -14,6 +17,7 @@ temp_hold = []
 temp_mean = []
 days = []
 months = ["Jan", "Feb", 'Mar', 'Apr', "May", 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
 
 for i in range(1): #gets the daily mean temperature over the span of 24 hours and so on
     for index, row in df.iterrows():
